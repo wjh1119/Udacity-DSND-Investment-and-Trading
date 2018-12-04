43dc848b621c9334496e6c39f5cc07029a7fda61
wjh1119 <524022279@qq.com>
2018-12-04 23:42:56 +0800

    feat: Add 'model.py' with some uncompleted function

---
+++ finance/model.py
@@ -0,0 +1,33 @@
+
+
+class StockModel():
+
+    def __init__(self):
+        '''initiate class
+        '''
+        pass
+
+    def train(self, X, y):
+        '''train model
+        '''
+        pass
+
+    def load(self, load_path=None):
+        '''load model
+        '''
+        pass
+
+    def save(self, save_path=None):
+        '''save model
+        '''
+        pass
+
+    def evaluate(self, X_test, y_test):
+        '''evaluate model
+        '''
+        pass
+
+    def predict(self, X):
+        '''predict
+        '''
+        pass
